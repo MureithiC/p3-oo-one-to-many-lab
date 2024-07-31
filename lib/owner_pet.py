62fc3bb948763ec021580f8e316f5f33fbba5408
Cyrus Mureithi <cmraey@gmail.com>
2024-07-31 14:14:50 +0300

    Done

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,25 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    def add_pet(self, pet):
+        if isinstance (pet, Pet):
+            pet.owner = self
+        else:
+            raise TypeError ("enter a Pet class object")
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key= lambda pet: pet.name)
+    
+class Pet:    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if not  pet_type in self.PET_TYPES:
+            raise TypeError("enter a type that is in the PET_TYPES list")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
